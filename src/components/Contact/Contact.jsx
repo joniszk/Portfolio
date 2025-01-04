6001f9740b94e8013a3d2f3281c9abd19a7b68c9
Jacob Oniszk <Jacob@jacobs-air-3.mynetworksettings.com>
2025-01-04 13:14:24 -0500

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:jacoboniszk@gmail.com">jacoboniszk@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/jacob-oniszk-8315bb298/">linkedin.com/JacobOniszk</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/joniszk">github.com/joniszk</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hi, I'm Jacob</h1>
+                <p className={styles.description}>
+                I'm a computer science major student, passionate about technology 
+                and dedicated to mastering coding and problem-solving.
+                </p>
+                <a href="mailto:jacoboniszk@gmail.com" className={styles.contactBtn}>
+                    Contact Me
+                </a>
+            </div>
+        <img src={getImageUrl("hero/me2.png")} alt="Image of me" className={styles.heroImg}/>
+            <div className={styles.topBlur} />
+            <div className={styles.bottomBlur} />
+    </section>
+    );
+};
